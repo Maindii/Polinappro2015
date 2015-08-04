4ce4a3a247c12b1b699049542e2e5037fddba17a
Maindi <maindi@kapsi.fi>
2015-08-04 23:50:02 +0300

    Hide menubar on splashscreen, fade it in on others

--- static/js/poli.js
+++ static/js/poli.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['splash', 'yleista', 'liput', 'aikataulu', 'kartta', 'muuta'],
-    menu: '#munMenu'
+    menu: '#munMenu',
+    onLeave: function(index, nextIndex, direction){
+      if(nextIndex == 1){
+        $(".navbar").fadeOut();
+      }else{
+        $(".navbar").fadeIn();
+      }
+    },
   });
+
+$(".navbar").hide();
+
 });
