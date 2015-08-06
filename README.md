ab37ca25a5346bc6d7e7dc9154f66438ba0cb5d8
Maindi <maindi@kapsi.fi>
2015-08-06 21:27:22 +0300

    asd

---
+++ README.md
@@ -0,0 +1,4 @@
+# Polinappro2015
+
+
+Simple single page website for Polin Appro 2015.
---
+++ static/js/poli.js
@@ -0,0 +1,39 @@
+function initialize() {
+  var myLatlng = new google.maps.LatLng(60.1642421, 24.9320541);
+  var mapOptions = {
+    zoom: 15,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+};
+
+var mapOn = false;
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['splash', 'yleista', 'liput', 'aikataulu', 'kartta', 'muuta'],
+    menu: '#munMenu',
+    scrollingSpeed: 1000,
+    onLeave: function(index, nextIndex, direction){
+      if(nextIndex == 1){
+        $(".navbar").fadeOut();
+      }else{
+        $(".navbar").fadeIn();
+      }
+    },
+    afterLoad: function(anchorLink, index){
+      if(!mapOn && index == 5){
+        mapOn=true;
+        initialize();
+      }
+    },
+  });
+
+$(".navbar").hide();
+
+});
