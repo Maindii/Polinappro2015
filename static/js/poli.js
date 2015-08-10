ade386f623d71108fa8f5a1d05a908bcf84ed980
Maindi <maindi@kapsi.fi>
2015-08-10 22:30:56 +0300

    add countdown timer

--- static/js/poli.js
+++ static/js/poli.js
@@ -35,5 +35,29 @@ $(document).ready(function() {
   });
 
 $(".navbar").hide();
+var end = new Date('11/5/2015 5:00 PM');
 
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+  var now = new Date();
+  var distance = end - now;
+  if (distance < 0) {
+    clearInterval(timer);
+    $('#countdown').text('EXPIRED!');
+
+    return;
+  }
+  var days = Math.floor(distance / _day);
+  var hours = Math.floor((distance % _day) / _hour);
+  var minutes = Math.floor((distance % _hour) / _minute);
+  var seconds = Math.floor((distance % _minute) / _second);
+  $('#countdown').text(days + " päivää " + hours + " tuntia " + minutes + " minuuttia " + seconds + " sekunttia ");
+}
+
+timer = setInterval(showRemaining, 1000);
 });
