75edb28a2d5f8c6d14a254261e6dd5f11b51a38d
kurkel <aleksi.asikainen@aalto.fi>
2015-08-24 22:54:29 +0300

    countdown

--- static/js/poli.js
+++ static/js/poli.js
@@ -5,16 +5,33 @@ jQuery(function($) {
     document.body.appendChild(script);
 });
 
+var day_text = " päivää ";
+var hour_text = " tuntia ";
+var minute_text = " minuuttia ";
+var seconds_text = " sekuntia ";
+
 function localize(lang) {
   var opts = {pathPrefix: "static/langs", language: lang};
   switch(lang){
     case "fi":
+      day_text = " päivää ";
+      hour_text = " tuntia ";
+      minute_text = " minuuttia ";
+      seconds_text = " sekuntia ";
       $(".navbar-nav li > a").css("font-size", "300%");
       break;
     case "en":
+      day_text = " days ";
+      hour_text = " hours ";
+      minute_text = " minutes ";
+      seconds_text = " seconds ";
       $(".navbar-nav li > a").css("font-size", "200%");
       break;
     default:
+      day_text = " dagar ";
+      hour_text = " timmar ";
+      minute_text = " minuter ";
+      seconds_text = " sekunder ";
       $(".navbar-nav li > a").css("font-size", "300%");
       break;
   }
@@ -92,7 +109,7 @@ function showRemaining() {
   var hours = Math.floor((distance % _day) / _hour);
   var minutes = Math.floor((distance % _hour) / _minute);
   var seconds = Math.floor((distance % _minute) / _second);
-  $('#countdown').text(days + " päivää " + hours + " tuntia " + minutes + " minuuttia " + seconds + " sekuntia ");
+  $('#countdown').text(days + day_text + hours + hour_text + minutes + minute_text + seconds + seconds_text);
 }
 
 timer = setInterval(showRemaining, 1000);
